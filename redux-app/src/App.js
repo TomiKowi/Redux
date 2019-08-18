23f145fc55d5f1a0e967e5bef7abdf0be15119b8
Tomek Kowol <tomek.pdp@gmail.com>
2019-08-18 12:37:30 +0200

    display counter with buttons

--- redux-app/src/App.js
+++ redux-app/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions'
+
+
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <h2>React-Redux</h2>
+      <h2>Counter: {counter}</h2>
+      <button onClick={() => dispatch(increment(6))}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      {isLogged ? <h2>Valuable information I should't see!</h2> : ""}
     </div>
   );
 }
