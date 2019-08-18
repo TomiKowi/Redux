fce131ba78480f9ea65f7ebdada3438eac4bbc28
Tomek Kowol <tomek.pdp@gmail.com>
2019-08-18 12:36:58 +0200

    add store & Provider

--- redux-app/src/index.js
+++ redux-app/src/index.js
@@ -2,40 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {
-    createStore
-} from 'redux';
+import { createStore } from 'redux';
+import allReducers from './reducers';
+import { Provider } from 'react-redux';
 
+const store = createStore(
+    allReducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-// ACTIONS
-
-const increment = () => {
-    return {
-        type: "INCREMENT"
-    }
-}
-const decrement = () => {
-    return {
-        type: "DECREMENT"
-    }
-}
-
-// REDUCER
-
-const counter = (state = 0, action) => {
-    switch (action.type) {
-        case "INCREMENT":
-            return state + 1;
-        case "DECREMENT":
-            return state - 1;
-    }
-}
-
-let store = createStore(counter);
-
-
-store.subscribe(() => console.log(store.getState()))
-
-store.dispatch(increment());
-
-ReactDOM.render( < App / > , document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        < App />
+    </Provider>,
+    document.getElementById('root'));
